d314431fcbb3b268478c696219c0962045fd1366
nilufar <nilufar@gmail.com>
2025-07-04 01:17:04 +0500

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import ProductList from "./pages/ProductList"; 
+import ProductList from "./pages/ProductList"; 
 import ProductDetail from "./pages/ProductDetail"; 
 
 function App() {
